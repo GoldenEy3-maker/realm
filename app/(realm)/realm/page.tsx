2149cfebb3a6124fdef30c652ff505234e56ed84
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2024-11-01 23:23:29 +0700

    refactor: add prefix "_" to realm segments

---
+++ app/(realm)/realm/page.tsx
@@ -0,0 +1,1 @@
+export { DashboardPage as default } from "@/pages/_realm/dahsboard";
--- src/app/layouts/root.tsx
+++ src/app/layouts/root.tsx
@@ -24,7 +24,7 @@ export function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ru">
+    <html lang="ru" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
