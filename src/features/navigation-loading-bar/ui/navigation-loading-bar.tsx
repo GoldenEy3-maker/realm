b36ebb03905b6d3c9ab8156f1d3dd233ef8442ce
Danil <danil.korolev@cyberia.studio>
2025-06-12 01:27:18 +0700

    fix(navigation-loading-bar): add check to show loading bar only if page is not visited

--- src/features/navigation-loading-bar/ui/navigation-loading-bar.tsx
+++ src/features/navigation-loading-bar/ui/navigation-loading-bar.tsx
@@ -1,18 +1,25 @@
-import { useRouterState } from "@tanstack/react-router";
+import { useLocation, useRouterState } from "@tanstack/react-router";
 import { useEffect, useRef } from "react";
 import LoadingBar, { LoadingBarRef } from "react-top-loading-bar";
 
 export function NavigationLoadingBar() {
   const loadingRef = useRef<LoadingBarRef>(null);
+  const location = useLocation();
   const status = useRouterState({ select: (state) => state.status });
+  const visitedRoutes = useRef<Set<string>>(new Set());
 
   useEffect(() => {
-    if (status === "pending") {
+    const currentPath = location.pathname;
+    const isVisited = visitedRoutes.current.has(currentPath);
+
+    if (status === "pending" && !isVisited) {
       loadingRef.current?.continuousStart();
     } else if (loadingRef.current?.getProgress() !== 0) {
       loadingRef.current?.complete();
     }
-  }, [status]);
+
+    if (!isVisited) visitedRoutes.current.add(currentPath);
+  }, [location.pathname, status]);
 
   return (
     <LoadingBar
